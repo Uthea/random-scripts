1133b915244d74806b3bde9c80c784a7911e8063
Gusti Sadhu Jnanin Werkodara <gstsadhu@gmail.com>
2020-03-02 20:52:27 +0700

    fix comment and path with spaces

--- rar/rar-fullpath-extract-notif.sh
+++ rar/rar-fullpath-extract-notif.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #this script requires rar and notify-send and used in thunar as custom action
-#this script will leave 1 nohup process in the background (also nohup.out in current folder)
+#this script will leave 1 tail process in the background (also nohup.out in current folder)
 
-killall -q nohup
-nohup rar x $1 &
+killall -q tail
+nohup rar x "$1" &
 tail -f nohup.out | while read line; do echo "$line" | grep -q -E "Extracting from |Total errors|All OK" && notify-send "Extracting RAR" "$line" -t 0 -h string:x-canonical-private-synchronous:my-notification; done   
 
 
